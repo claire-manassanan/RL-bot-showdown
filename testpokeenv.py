2016d3d9ce22c138dfdf7acbfe91bb09a0396806
claire-johnsons <slothyeiei@gmail.com>
2024-12-15 12:29:44 +0700

    edit team.txt and requirements

--- testpokeenv.py
+++ testpokeenv.py
@@ -11,12 +11,14 @@ from poke_env.player import Player
 from poke_env.environment.battle import Battle
 from poke_env.teambuilder import Teambuilder
 
-team_file = open('team.txt')
-team_player = team_file.read()
-all_team = team_player.split("***split***")
+team1_file = open('team1.txt')
+team2_file = open('team2.txt')
+team_1 = team1_file.read()
+team_2 = team2_file.read()
 
-team_usa = all_team[0]
-team_thai = all_team[1]
+team1_file.close()
+team2_file.close()
+
 class RandomTeamFromPool(Teambuilder):
     def __init__(self, teams):
         self.packed_teams = []
@@ -28,68 +30,41 @@ class RandomTeamFromPool(Teambuilder):
 
     def yield_team(self):
         return np.random.choice(self.packed_teams)
-    
 
-# teams = [team_usa, team_thai]
-
-# team = RandomTeamFromPool(teams)
-# print(sys.path)
 
 server = ServerConfiguration(
     authentication_url="http://localhost:8000/showdown/action.php",
     websocket_url="ws://localhost:8000/showdown/websocket"
 )
 
+format = 'gen9randombattle'
 p1 = RandomPlayer(
-    battle_format='gen9vgc2024regh',
-    server_configuration=server,
-    team=team_thai
+    battle_format=format,
+    server_configuration=server
+    # team=team_1
 )
 p2 = RandomPlayer(
-    battle_format='gen9vgc2024regh',
-    server_configuration=server,
-    team=team_usa
+    battle_format=format,
+    server_configuration=server
+    # team=team_2
 )
 
-
-class MaxDamagePlayer(RandomPlayer):
+class MaxDamagePlayer(Player):
     def choose_move(self, battle):
-        # Chooses a move with the highest base power when possible
         if battle.available_moves:
+            print(f"Turn: {battle.turn}")
             # Iterating over available moves to find the one with the highest base power
             best_move = max(battle.available_moves, key=lambda move: move.base_power)
+            print(best_move,end='\n')
             # Creating an order for the selected move
             return self.create_order(best_move)
         else:
-            # If no attacking move is available, perform a random switch
-            # This involves choosing a random move, which could be a switch or another available action
             return self.choose_random_move(battle)
         
-class CustomPlayer(Player):
-    def choose_move(self, battle):
-        # Log some basic battle info
-        print(f"Turn: {battle.turn}")
-        # print(f"My active Pokémon: {battle.active_pokemon}")
-        # print(f"Opponent's active Pokémon: {battle.opponent_active_pokemon}")
-
-        if battle.available_moves:
-            # Log available moves
-            print(f"Available moves: {[move.id for move in battle.available_moves]}")
-            
-            # Example strategy: Use the first available move
-            return self.create_order(battle.available_moves[0])
-        
-        # Check available switches
-        if battle.available_switches:
-            print(f"Available switches: {[poke.species for poke in battle.available_switches]}")
-            
-            # Example strategy: Switch to the first available Pokémon
-            return self.create_order(battle.available_switches[0])
-        
-        # Default to a random move
-        return self.choose_random_move()
-
-# maxx = MaxDamagePlayer(team=team_usa)
+maxx = MaxDamagePlayer(
+    battle_format=format
+    # team=team_2
+)
 # custom = CustomPlayer(
 #     battle_format='gen9vgc2024regh',
 #     team=team_usa
@@ -98,11 +73,7 @@ class CustomPlayer(Player):
 async def vgc2024(p1,p2):
     await p1.battle_against(p2, n_battles=1)
 
-asyncio.run(vgc2024(p1,p2))
-# async def battle_basic(maxx,p1):
-#     await p1.battle_against(maxx, n_battles=1)
-team_file.close()
-# asyncio.run(battle_basic(p2,p1))
+asyncio.run(vgc2024(p1,maxx))
 
 # print(f'max won {maxx.n_won_battles} times')
 # print(f'p1 won {p1.n_won_battles} times')
